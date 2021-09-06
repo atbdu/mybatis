352632fc63642f76677048b0abc698bf02159460
Administrator <2741217952@qq.com>
2021-09-06 16:27:19 +0800

    mybatis项目基本机构

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -18,13 +18,23 @@ public class Test {
     public static void main(String[] args) throws IOException {
         SqlSession sqlSession = MybatisUtils.getSqlSession();
         DayMapper mapper = sqlSession.getMapper(DayMapper.class);
-        Day day = mapper.getDay();
-        System.out.println(day);
-        Day day1 = new Day();
-        day1.setDay(new Date(System.currentTimeMillis()));
-        day1.setCode(1);
-        int i = mapper.updateDay(day1);
-        System.out.println(i);
+//        //查询
+//        Day day = mapper.getDay();
+//        System.out.println(day);
+//        //修改
+//        Day day1 = new Day();
+//        day1.setDay(new Date(System.currentTimeMillis()));
+//        day1.setCode(1);
+//        int i = mapper.updateDay(day1);
+//        System.out.println(i);
+        //增加
+//        Day day2 = new Day();
+//        day2.setCode(3);
+//        day2.setDay(new Date(System.currentTimeMillis()));
+//        int m = mapper.insertDay(day2);
+//        System.out.println(m);
+        //删除
+        mapper.deleteDay(3);
         //提交
         sqlSession.commit();
         //关闭资源
--- src/main/java/mapper/DayMapper.java
+++ src/main/java/mapper/DayMapper.java
@@ -5,5 +5,8 @@ import pojo.Day;
 public interface DayMapper {
     public Day getDay();
     public int updateDay(Day day);
+    public void deleteDay(int code);
+    public int insertDay(Day day);
+
 }
 
